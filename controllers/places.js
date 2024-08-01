d789962c6659041d67415d0b2b67c1f2fec861bf
Saad Shafiq <saadshafiq259@gmail.com>
2024-08-01 11:59:09 +0500

    get all places

--- controllers/places.js
+++ controllers/places.js
@@ -6,7 +6,7 @@ const pool = require("../db/connection");
 
 const placesTable = "places";
 
-const getPlaces = async (req, res) => {
+const getPlacesByRadius = async (req, res) => {
   const { lat, lon, radius = 200 } = req.query;
   if (!lat || !lon) {
     throw new BadRequestError(
@@ -46,4 +46,23 @@ const getPlaces = async (req, res) => {
   res.status(StatusCodes.OK).json({ places: result.recordset });
 };
 
-module.exports = { getPlaces };
+const getPlaces = async (req, res) => {
+  const { limit = 500, offset = 0 } = req.query;
+  const poolResult = await pool;
+
+  const request = poolResult.request();
+
+  const query = `
+    SELECT * FROM ${placesTable}
+    ORDER BY id
+    OFFSET @offset ROWS FETCH NEXT @limit ROWS ONLY;
+  `;
+  request.input("limit", mssql.Int, limit);
+  request.input("offset", mssql.Int, offset);
+
+  const result = await request.query(query);
+
+  res.status(StatusCodes.OK).json({ places: result.recordset });
+};
+
+module.exports = { getPlacesByRadius, getPlaces };
--- routes/places.js
+++ routes/places.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getPlaces } = require("../controllers/places");
+const { getPlacesByRadius, getPlaces } = require("../controllers/places");
 
+router.route("/radius").get(getPlacesByRadius);
 router.route("/").get(getPlaces);
 
 module.exports = router;
